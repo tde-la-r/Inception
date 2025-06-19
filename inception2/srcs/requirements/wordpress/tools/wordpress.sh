fcfbc20bba501c99e0ab70c061e6bfc97a4dcc29
tde-la-r <theaudelaruelle@yahoo.fr>
2025-06-19 11:23:45 +0200

    wordpress fixed needs test with mariadb

--- inception2/srcs/requirements/wordpress/tools/wordpress.sh
+++ inception2/srcs/requirements/wordpress/tools/wordpress.sh
@@ -12,17 +12,21 @@ USER_MAIL=theau_mail
 
 # Configure WordPress if it isn't already set
 if [ ! -f /var/www/html/wp-config.php ]; then
-    cd  /var/www/html/
-    if [ ! -f index.php ]; then
-        wp-cli.phar core download
-    fi
+	cd  /var/www/html/
+	if [ ! -f index.php ]; then
+		echo "[WP_CONFIG] Downloading wordpress..."
+		wp-cli.phar core download --allow-root
+	fi
 	# Create the base configuration file
-	wp-cli.phar config create   --dbname="$MARIADB_DATABASE" --dbuser="$MARIADB_USER" --dbpass="$MARIADB_PASSWORD" --dbhost=mariadb
+	echo "[WP_CONFIG] Creating the base configuration file..."
+	wp-cli.phar config create   --dbname="$MARIADB_DATABASE" --dbuser="$MARIADB_USER" --dbpass="$MARIADB_PASSWORD" --dbhost=mariadb --allow-root
 	
 	# Create the admin user 
-	wp-cli.phar core install    --url="tde-la-r.42.fr" --title="tde-la-r-Inception" --admin_user="$ADMIN_USR" --admin_password="$ADMIN_PWD" --admin_email="$ADMIN_MAIL"
+	echo "[WP_CONFIG] Creating the admin user ..."
+	wp-cli.phar core install    --url="tde-la-r.42.fr" --title="tde-la-r-Inception" --admin_user="$ADMIN_USR" --admin_password="$ADMIN_PWD" --admin_email="$ADMIN_MAIL" --allow-root
 	# Create the second user
-	wp-cli.phar user create     "$USER_USR" "$USER_MAIL" --role=author --user_pass="$USER_PWD"
+	echo "[WP_CONFIG] Creating the admin user ..."
+	wp-cli.phar user create     "$USER_USR" "$USER_MAIL" --role=author --user_pass="$USER_PWD" --allow-root
 
 	# Give the User the own of the folder
 	#chown -R ${GL_USER}:${GL_USER} /var/www/html
