da7ab3028e6edde5158b4e31b6da6e042774c3c3
tde-la-r <theaudelaruelle@yahoo.fr>
2025-06-23 15:49:41 +0200

    clearing project

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -8,46 +8,34 @@ if [ ! -d "/run/mysqld" ]; then
 	chown -R mysql:mysql /run/mysqld
 fi
 
-#if [ -d "${MARIADB_DATADIR}" ]
-#then
-	#echo "[DB CONFIG] MariaDB already configured."
-#else
-	echo "[DB CONFIG] Installing MySQL Data Directory..."
-	mkdir ${MARIADB_DATADIR} 
-	chown -R mysql:mysql ${MARIADB_DATADIR}
-	mariadb-install-db --basedir=/usr --datadir=${MARIADB_DATADIR} --user=mysql --rpm
-	echo "[DB CONFIG] MySQL Data Directory done."
+echo "[DB CONFIG] Installing MySQL Data Directory..."
+mkdir -p ${MARIADB_DATADIR} 
+chown -R mysql:mysql ${MARIADB_DATADIR}
+mariadb-install-db --basedir=/usr --datadir=${MARIADB_DATADIR} --user=mysql --rpm
+echo "[DB CONFIG] MySQL Data Directory done."
 
-	echo "[DB CONFIG] Configuring MySQL..."
-	TMP=/tmp/.tmpfile
+echo "[DB CONFIG] Configuring MySQL..."
+TMP=/tmp/.tmpfile
 
-	echo "USE mysql;" >> ${TMP}
-	echo "FLUSH PRIVILEGES;" >> ${TMP}
-	echo "DELETE FROM mysql.user WHERE User='';" >> ${TMP}
-	echo "DROP DATABASE IF EXISTS test;" >> ${TMP}
-	echo "DELETE FROM mysql.db WHERE Db='test';" >> ${TMP}
-	echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" >> ${TMP}
-	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';" >> ${TMP}
-	#echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}' WITH GRANT OPTION;" >> ${TMP}
-	#echo "GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}' WITH GRANT OPTION;" >> ${TMP}
-	echo "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};" >> ${TMP}
-	echo "CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';" >> ${TMP}
-	echo "GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';" >> ${TMP}
-	echo "FLUSH PRIVILEGES;" >> ${TMP}
+echo "USE mysql;" >> ${TMP}
+echo "FLUSH PRIVILEGES;" >> ${TMP}
+echo "DELETE FROM mysql.user WHERE User='';" >> ${TMP}
+echo "DROP DATABASE IF EXISTS test;" >> ${TMP}
+echo "DELETE FROM mysql.db WHERE Db='test';" >> ${TMP}
+echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" >> ${TMP}
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';" >> ${TMP}
+echo "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};" >> ${TMP}
+echo "CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';" >> ${TMP}
+echo "GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';" >> ${TMP}
+echo "FLUSH PRIVILEGES;" >> ${TMP}
 
-	# Alpine does not come with service or rc-service,
-	# so we cannot use: service mysql start
-	# We might be able to install with: apk add openrc
-	# But we can also manually start and configure the mysql daemon:
-	mariadbd --user=mysql --datadir=${MARIADB_DATADIR} --bootstrap < ${TMP}
-	rm -f ${TMP}
-	echo "[DB CONFIG] MySQL configuration done."
-#fi
+mariadbd --user=mysql --datadir=${MARIADB_DATADIR} --bootstrap < ${TMP}
+rm -f ${TMP}
+echo "[DB CONFIG] MySQL configuration done."
 
 echo "[DB CONFIG] Allowing remote connections to MariaDB"
 sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
 echo "[DB CONFIG] Starting MariaDB daemon on port 3306."
-#chown -R 999:999 /var/lib/docker/volumes/srcs_mariadb/_data
 exec mariadbd --datadir=${MARIADB_DATADIR} --user=mysql --console
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -7,19 +7,33 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 		echo "[WP_CONFIG] Downloading wordpress..."
 		wp-cli.phar core download --allow-root
 	fi
+
 	# Create the base configuration file
 	echo "[WP_CONFIG] Creating the base configuration file..."
-	wp-cli.phar config create   --dbname="$MARIADB_DATABASE" --dbuser="$MARIADB_USER" --dbpass="$MARIADB_PASSWORD" --dbhost=mariadb --allow-root
+	wp-cli.phar config create \
+		--dbname="$MARIADB_DATABASE" \
+		--dbuser="$MARIADB_USER" \
+		--dbpass="$MARIADB_PASSWORD" \
+		--dbhost=mariadb \
+		--allow-root
 	
 	# Create the admin user 
 	echo "[WP_CONFIG] Creating the admin user ..."
-	wp-cli.phar core install    --url="tde-la-r.42.fr" --title="tde-la-r-Inception" --admin_user="$ADMIN_USR" --admin_password="$ADMIN_PWD" --admin_email="$ADMIN_MAIL" --allow-root
+	wp-cli.phar core install \
+		--url="tde-la-r.42.fr" \
+		--title="tde-la-r-Inception" \
+		--admin_user="$ADMIN_USR" \
+		--admin_password="$ADMIN_PWD" \
+		--admin_email="$ADMIN_MAIL" \
+		--allow-root
+
 	# Create the second user
 	echo "[WP_CONFIG] Creating the admin user ..."
-	wp-cli.phar user create     "$USER_USR" "$USER_MAIL" --role=author --user_pass="$USER_PWD" --allow-root
+	wp-cli.phar user create "$USER_USR" "$USER_MAIL" \
+		--role=author \
+		--user_pass="$USER_PWD" \
+		--allow-root
 
-	# Give the User the own of the folder
-	#chown -R ${GL_USER}:${GL_USER} /var/www/html
 fi
 
 # Start PhP
